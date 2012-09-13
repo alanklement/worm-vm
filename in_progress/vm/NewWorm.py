0d5f4ab32d5d10f2fb0634dfc5974456bdf8dc9c
Alan Klement <alan.m.klement@gmail.com>
2012-09-13 18:33:31 -0400

    Clarifed which VM was old.

--- in_progress/vm/NewWorm.py
+++
@@ -1,9 +0,0 @@
-from Program import Program
-from ArguementReader import ArguementReader
-
-argparser = ArguementReader()
-argparser.listenForArguements()
-
-program = Program()
-program.byteCodes = argparser.file.read().splitlines()
-program.execute()
